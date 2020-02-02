1b1bf0aab1dd6a9a2d04b15bc3e3e897584ee995
DaibhidhBoyle <daibhidhboyle@gmail.com>
2020-02-02 07:21:37 +0000

    now recieve error message when you shoutout but dont specify someone

--- src/models/shoutout.js
+++ src/models/shoutout.js
@@ -24,35 +24,46 @@ Shoutout.prototype.bindShoutout = function () {
 
   PubSub.subscribe(SHOUTOUT, (msg, data) => {
 
-    this.shoutoutList = [];
-
     this.message = data
 
-    let counter = 0
+    if (this.message === '!so'){
 
+      this.response = `Looks like your trying to shout someone out but you haven't said who. If you want to shout someone out be sure to @ them`;
 
-
-    while (counter < 3){
-      this.slicemessage(this.message);
-      this.test = this.test + ` ` + `${counter}`
-      this.message = this.str3;
-      counter++
-    };
-
-    let arrayLength = this.shoutoutList.length;
-
-    if(arrayLength === 1){
-      this.response = `Looking for a new stream to follow? Try    *** www.twitch.tv/${this.shoutoutList[0]} ***`
-    } else if (arrayLength === 2) {
-      this.response = `They're some great streamers out there! Checkout  *** www.twitch.tv/${this.shoutoutList[0]} ***  or  *** www.twitch.tv/${this.shoutoutList[1]} ***`
-    } else if (arrayLength === 3) {
-      this.response = `Don't let your time on twitch when the the Earl of Suds logs out. Checkout out more great streamers like *** www.twitch.tv/${this.shoutoutList[0]} *** or  *** www.twitch.tv/${this.shoutoutList[1]} *** or  *** www.twitch.tv/${this.shoutoutList[2]} ***`
+      PubSub.publish(MODONLYRESPONSE, this.response);
     } else {
-      this.response = `Looks like your trying to shout someone out but you haven't said who. If you want to shout someone out be sure to @ them`
+      this.shoutoutList = [];
+
+      let counter = 0
+
+
+
+      while (counter < 3){
+        this.slicemessage(this.message);
+        this.test = this.test + ` ` + `${counter}`
+        this.message = this.str3;
+        counter++
+      };
+
+      let arrayLength = this.shoutoutList.length;
+
+      if(arrayLength === 1){
+        this.response = `Looking for a new stream to follow? Try    *** www.twitch.tv/${this.shoutoutList[0]} ***`
+      } else if (arrayLength === 2) {
+        this.response = `They're some great streamers out there! Checkout  *** www.twitch.tv/${this.shoutoutList[0]} ***  or  *** www.twitch.tv/${this.shoutoutList[1]} ***`
+      } else if (arrayLength === 3) {
+        this.response = `Don't let your time on twitch end when the the Earl of Suds logs out. Checkout out more great streamers like *** www.twitch.tv/${this.shoutoutList[0]} *** or  *** www.twitch.tv/${this.shoutoutList[1]} *** or  *** www.twitch.tv/${this.shoutoutList[2]} ***`
+      } else {
+        this.response = `Looks like your trying to shout someone out but you haven't said who. If you want to shout someone out be sure to @ them`
+      };
+
+      PubSub.publish(MODONLYRESPONSE, this.response);
     };
 
-    PubSub.publish(MODONLYRESPONSE, this.response);
   });
+
+
+
   Shoutout.prototype.slicemessage = function (str) {
 
     this.num1 = str.indexOf(`@`);
