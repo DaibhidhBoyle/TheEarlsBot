e421d163bc95f6ba8879d3600179fa4803d12bdb
DaibhidhBoyle <daibhidhboyle@gmail.com>
2020-02-04 07:52:01 +0000

    changes to streak

--- src/models/streak.js
+++ src/models/streak.js
@@ -8,9 +8,6 @@ const Streak = function (){
   this.count = null
 };
 
-const STREAK = 'ChatBot: chat message to be replied to by how days in a row the stream has streamed';
-const RESPONSE = '*: ready response to Twitch';
-
 Streak.prototype.bindStreak = function () {
 
   storage.initSync();
