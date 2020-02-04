ca732f51458ca14d05c8d0c81a9877ab54fb98bb
DaibhidhBoyle <daibhidhboyle@gmail.com>
2020-02-04 06:38:38 +0000

    streak functional

--- src/models/streak.js
+++ src/models/streak.js
@@ -13,12 +13,6 @@ const RESPONSE = '*: ready response to Twitch';
 Streak.prototype.bindStreak = function () {
 
   storage.initSync();
-//fake
-// let pseudo = new Date();
-// pseudo.setHours(0,0,0,0);
-// pseudo.setDate(pseudo.getDate() - 1);
-// storage.setItemSync(`${pseudo}`, 3);
-// end
 
   let today = new Date();
   today.setHours(0,0,0,0);
@@ -29,21 +23,45 @@ Streak.prototype.bindStreak = function () {
   if(record !== undefined){
     record ++
     storage.removeItemSync(`${yesterday}`);
-     storage.setItemSync(`${today}`, record);
-     this.count = storage.getItemSync(`${today}`)
-} else {
-  storage.forEach(function(key, value) {
-  storage.removeItemSync(`${key}`);
-});
-storage.setItemSync(`${today}`, 1);
-this.count = storage.getItemSync(`${today}`)
+    storage.setItemSync(`${today}`, record);
+    this.count = storage.getItemSync(`${today}`)
+  } else {
+    storage.forEach(function(key, value) {
+      storage.removeItemSync(`${key}`);
+    });
+    storage.setItemSync(`${today}`, 1);
+    this.count = storage.getItemSync(`${today}`)
 
-}
+  }
 
 
   PubSub.subscribe(STREAK, (msg, data) => {
+    if(this.count === 1){
+      this.response = `Kenny is just starting a new streak! This is day 1`;
+    }
+    else if (this.count === 2 || this.count === 3){
+      this.response = `We're just getting started. Current streak is ${this.count} days!`;
+    }
+    else if (this.count >= 4 && this.count <7){
+        this.response = `Things are heating up! Kennys streak is ${this.count} days in a row`;
+      }
+      else if (this.count === 7){
+          this.response = `Wow! The Earls has streamed every day for a week straight!`;
+        }
+        else if (this.count >= 8 && this.count <14){
+            this.response = `Kenny current streaming streak is a blazing ${this.count} days in a row!`;
+          }
+          else if (this.count === 14){
+              this.response = `Good Golly! Kenny has streamed every day for a fortnight!`;
+            }
+            else if (this.count >= 14 && this.count < 25){
+               this.response = `The Earl's on a hot streak; he's streamed for a mighty ${this.count} days in a row!`;
+             }
+             else if (this.count >= 25){
+                 this.response = `Hot Dang! The Earl of Suds has streamed everyday for ${this.count} days! Now that's impressive.`;
+               };
 
-    PubSub.publish(RESPONSE, this.count);
+    PubSub.publish(RESPONSE, this.response);
   });
 };
 
