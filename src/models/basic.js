f05229030373909d5c78d535b9311c3a41d5a9a3
DaibhidhBoyle <daibhidhboyle@gmail.com>
2021-01-11 22:02:37 +0000

    fix time on when command

--- src/models/basic.js
+++ src/models/basic.js
@@ -21,7 +21,7 @@ Basic.prototype.bindBasic = function () {
       this.response = `Kenny and his family work to make luxury, hand-crafted soaps and invites you be part of the process through high quality crafting videos. Find out more at https://www.royaltysoaps.com/ or get a look at those 'making of' videos at youtube.com/royaltysoaps`
     }
     else if(this.message.includes('!when') || data.includes('!schedule')){
-      this.response = `Catch the stream 6pm CT everyday (cept sunday)! and  special early Sunday stream at 1pm CT. Keep up to date on any schedule changes at the discord: discordapp.com/invite/PmK33d4`
+      this.response = `Catch the stream 4pm CT everyday (cept sunday)! and  special early Sunday stream at 1pm CT. Keep up to date on any schedule changes at the discord: discordapp.com/invite/PmK33d4`
     }
     else if(this.message.includes('!movie') || data.includes('!mn')){
       this.response = `Subs! Join us on Discord ( discordapp.com/invite/PmK33d4 ) for a Movie Night every Sunday at 6pm CT. Remember to connect your Discord to your Twitch using the Connections tab so you can be part of the fun!`
